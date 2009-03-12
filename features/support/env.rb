273ea00412ed3bc2c89902c5342189d56688581e
David Richards <davidlamontrichards@gmail.com>
2009-03-12 13:29:27 -0600

    Initial commit to tegu_gears.

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'tegu_gears'
+
+require 'spec/expectations'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world
+end
